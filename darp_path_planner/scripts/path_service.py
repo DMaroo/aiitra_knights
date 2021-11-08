6cf4c54f275e8881b2034de929b73a3ef904043b
Dhruv Maroo <dhruvmaru007@gmail.com>
2021-11-09 00:04:29 +0530

    Minor changes

--- darp_path_planner/scripts/path_service.py
+++ darp_path_planner/scripts/path_service.py
@@ -6,10 +6,11 @@ from darp_path_planner.srv import DARPPath, DARPPathResponse
 
 import rospy
 from darpinPoly import DARPinPoly
+import sys
 
 def serve_path(request):
     rospy.logdebug(f"Serving path for: mode = {request.mode} | bot = {request.bot} | step = {request.step}")
-    return DARPPathResponse(poly[request.mode][request.bot][request.step][2], poly[request.mode][request.bot][request.step][3], rospy.Time.now())
+    return DARPPathResponse(poly.final_paths[request.mode][request.bot][request.step][2], poly.final_paths[request.mode][request.bot][request.step][3], rospy.Time.now())
 
 def server():
     rospy.init_node('path_service')
@@ -26,9 +27,50 @@ if __name__ == '__main__':
     dcells = rospy.get_param("dcells", 2)
     importance = rospy.get_param("importance", False)
     nep = rospy.get_param("nep", False)
-    initial_positions = rospy.get_param("initial_positions", [[0, 1], [0, 3], [0, 9]])
-    portions = rospy.get_param("portions", [0.2, 0.3, 0.5])
-    obstacles_positions = rospy.get_param("obstacles_positions", [])
+    p_initial_positions = rospy.get_param("initial_positions", [1, 3, 9])
+    p_portions = rospy.get_param("portions", [0.2, 0.3, 0.5])
+    p_obstacles_positions = rospy.get_param("obstacles_positions", [])
+
+    obstacles_positions = []
+    initial_positions = []
+
+    for position in p_initial_positions:
+        if position < 0 or position >= rows*cols:
+            print("Initial positions should be inside the Grid.")
+            sys.exit(2)
+        initial_positions.append((position // cols, position % cols))
+        #saving position input to print for path optimization simulation results
+        pos_in_long = p_initial_positions
+
+    # finding row and column of obstalces position from grid position
+    for obstacle in p_obstacles_positions:
+        if obstacle < 0 or obstacle >= rows*cols:
+            print("Obstacles should be inside the Grid.")
+            sys.exit(3)
+        obstacles_positions.append((obstacle // cols, obstacle % cols))
+
+    portions = []
+    if nep:
+        for portion in p_portions:
+            portions.append(portion)
+    else:
+        for drone in range(len(initial_positions)):
+            portions.append(1/len(initial_positions))
+
+    if len(initial_positions) != len(portions):
+        print("Portions should be defined for each drone")
+        sys.exit(4)
+
+    s = sum(portions)
+    if abs(s-1) >= 0.0001:
+        print("Sum of portions should be equal to 1.")
+        sys.exit(1)
+
+    for position in initial_positions:
+        for obstacle in obstacles_positions:
+            if position[0] == obstacle[0] and position[1] == obstacle[1]:
+                print("Initial positions should not be on obstacles")
+                sys.exit(3)
 
     poly = DARPinPoly(rows, cols, maxIter, CCvariation, randomLevel, dcells, importance, nep, initial_positions, portions, obstacles_positions)
 
